dbc962476b70d37cd2ae3052c9be6b838437cefb
AnotherSubatomo <secretsubatomo@gmail.com>
2026-04-12 17:22:48 +0800

    web renderer finally working

--- renderer/web/index.js
+++ renderer/web/index.js
@@ -3,34 +3,55 @@ import { LuauState } from 'https://cdn.jsdelivr.net/gh/xNasuni/luau-web@main/dis
 const state = await LuauState.createAsync({
 	// Quick-n-dirty fake require function
 	require: async function (path) {
-		path = path.replace("./", "")
+		path = path.replace("../", "").replace("./", "")
 
 		if (path == "kosmos") {
 			path = "../../src/init.luau"
+		} else if (path == "demo") {
+			path = "../../demo/init.luau"
 		} else {
 			path = "../../" + path + ".luau"
 		}
 
-		const source = await fetch(path).then(r => r.text());
-		const main = state.loadstring(source, path, true);
-		const result = await main();
+		const source = await fetch(path).then(r => r.text())
+		const main = state.loadstring(source, path, true)
+		const result = await main()
 
-		//console.log(result)
-		return result;
+		return result
 	}
 });
 
-const demo_source = await fetch('../../demo/init.luau').then(r => r.text());
-const demo_main = state.loadstring(demo_source, "kosmos-demo", true);
-const program = await demo_main();
+const demo_source = await fetch('./init.luau').then(r => r.text())
+const demo_main = state.loadstring(demo_source, "kosmos-demo", true)
+const program = await demo_main()
 
-// NOTE: Discontinued & not working as of now because of confusion
-// on how the module actually works, the library author has been asked
-// and a response is yet to be received.
- 
-console.log(program);
-// Outputs: [Proxy(Object)]
-// Okay... but,
-console.log(program.width, program.height, program.frame);
-// Expected output: 320, 240, frame()
-// Actual output: undefined, undefined, undefined
+const width = program[0]
+const height = program[1]
+const frame = program[2]
+
+canvas.width = width
+canvas.height = height
+
+const ctx = canvas.getContext("2d")
+const image_data = ctx.createImageData(width, height)
+const pixels = new Uint8ClampedArray(width * height * 4)
+
+let last_time = 0
+
+async function loop(time) {
+	let dt = (time - last_time) / 1000
+	last_time = time
+
+	let image_string = (await frame(dt))[0]
+
+	for (let i = 0; i < pixels.length; i++) {
+		pixels[i] = image_string.charCodeAt(i)
+	}
+
+	image_data.data.set(pixels)
+	ctx.putImageData(image_data, 0, 0)
+
+	requestAnimationFrame(loop)
+}
+
+requestAnimationFrame(loop)
